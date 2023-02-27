b16f5ff9cddb5b975ced25f4e1ca7bf6b1badd63
Hamid Hussain <hamidhussain740@gmail.com>
2023-02-28 01:10:31 +1100

    start on date 27 feb 1pm so total 1 day and 6 hours spend

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <nav className="mycustomNavBar navbar navbar-expand bg-body-tertiary fixed-top">
         <div className="container-fluid myheader">
           <div className="container">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="/mvp">
             <img src={logo} alt="logo" style={logoStyle}/>
           </a>
           <button
@@ -37,7 +37,7 @@ const Header = () => {
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/" style={headerStyle}>
+                <a className="nav-link active" aria-current="page" href="/mvp" style={headerStyle}>
                   AR
                 </a>
               </li>
@@ -52,14 +52,14 @@ const Header = () => {
                 Menu
               </button>
               {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="/mvp" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
+                  <li><a className="dropdown-item" href="/mvp">Action</a></li>
+                  <li><a className="dropdown-item" href="/mvp">Another action</a></li>
                   <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
+                  <li><a className="dropdown-item" href="/mvp">Something else here</a></li>
                 </ul>
               </li> */}
             </ul>
@@ -94,9 +94,9 @@ const Header = () => {
               </li>
               </Link>
               <Link to="/aboutus"><li className="list-group-item">About Us</li></Link>
-              <a href="/"><li className="list-group-item">Shop</li></a>
-              <a href="/"><li className="list-group-item">Login</li></a>
-              <a href="/"><li className="list-group-item">Register</li></a>
+              <a href="/mvp"><li className="list-group-item">Shop</li></a>
+              <a href="/mvp"><li className="list-group-item">Login</li></a>
+              <a href="/mvp"><li className="list-group-item">Register</li></a>
             </ul>
           </div>
         </div>
