b7f93d853cc79094773782fea07239cdf44ce786
Hamid Hussain <hamidhussain740@gmail.com>
2023-02-27 18:06:20 +1100

    6 hours spend

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -79,12 +79,12 @@ const Header = () => {
           <div className="offcanvas-body">
             <ul className="list-group">
               <li className="list-group-item active" aria-current="true">
-                An active item
+              <a href="/">Start Creating</a>
               </li>
-              <li className="list-group-item">A second item</li>
-              <li className="list-group-item">A third item</li>
-              <li className="list-group-item">A fourth item</li>
-              <li className="list-group-item">And a fifth one</li>
+              <li className="list-group-item"><a href="/">About Us</a></li>
+              <li className="list-group-item"><a href="/">Shop</a></li>
+              <li className="list-group-item"><a href="/">Login</a></li>
+              <li className="list-group-item"><a href="/">Register</a></li>
             </ul>
           </div>
         </div>
