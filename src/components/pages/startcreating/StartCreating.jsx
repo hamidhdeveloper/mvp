0a378dc4a9539266f346781025800ccbee6ca00d
Hamid Hussain <hamidhussain740@gmail.com>
2023-02-27 23:53:16 +1100

    1 day and 6 hours spend

--- src/components/pages/startcreating/StartCreating.jsx
+++ src/components/pages/startcreating/StartCreating.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import ImageCarousel from "./ImageCarousel";
+import ImageCarousel from "./ImageCarousel";
 import "./StartCreating.scss";
 
 const StartCreating = () => {
   return (
     <>
-      <div class="container-fluid mycreating">
-        <div class="row">
-          <div class="col-md-12 mycreatingcol">
+      <div className="container-fluid mycreating">
+        <div className="row">
+          <div className="col-md-12 mycreatingcol">
             <h1 className="text-center text-sm-1">
               St<span style={{ color: "#FEC30E" }}>art</span>
             </h1>
@@ -33,9 +33,9 @@ const StartCreating = () => {
         </div>
       </div>
       <div className="container-fulid mycarouselControls">
-        <div class="row">
-          <div class="col-md-12">
-            {/* <ImageCarousel /> */}
+        <div className="row">
+          <div className="col-md-12">
+            <ImageCarousel />
           </div>
         </div>
       </div>
